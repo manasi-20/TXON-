364b66aaacb9226757755f135d70be38f3e5cbea
Manasi Patil <110348004+manasi-20@users.noreply.github.com>
2023-01-27 22:48:22 +0530

    Create QuestionAnswer.java

---
+++ QuizApp/QuestionAnswer.java
@@ -0,0 +1,39 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+        public static String question[]={
+                "Which gun has the most recoil ?",
+                "Pubg is originated from which country ?",
+                "Name a famous sniper in PUBG.",
+                "Which is the largest map in Pubg ?",
+                "UZI has a mag(magazine size) of ",
+                "UMP45 can have an extended magazine of ",
+                "Which of the following is a sub-machine gun ?",
+                "AKM is used with ",
+                "Which bullets are used in UZI ?",
+                "Which level is the highest level in Pubg ?"
+};
+        public static String choices[][]={
+                {"M416","Scar l","AKM","M24"},
+                {"China","South Korea","North Korea","Pakistan"},
+                {"AWM","Scar l","AKM","M24"},
+                {"Livik","Erangel","Miramar","Erangel and Miramar"},
+                {"40 Bullets","35 Bullets","25 Bullets","20 Bullets"},
+                {"40 Bullets","35 Bullets","25 Bullets","Can't be extended"},
+                {"UMP45","Scar l","AKM","M24"},
+                {"Holographic Scope","2x","4x or 6x","8x"},
+                {"10 mm","9 mm","4 mm","12 mm"},
+                {"Platinum","Crown","Grand Master","Diamond"}
+};
+        public static String correct[]={
+                "AKM",
+                "South Korea",
+                "AWM",
+                "Erangel and Miramar",
+                "25 BUllets",
+                "35 Bullets",
+                "UMP45",
+                "Holographic scope",
+                "9mm",
+                "Grand Master"
+};
